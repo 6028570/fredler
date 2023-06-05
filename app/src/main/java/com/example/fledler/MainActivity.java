0e616d04cbe1a889e9b51cd8070bd3f577af0b53
mohammedcifci <118259207+mohammedcifci@users.noreply.github.com>
2023-06-05 11:41:25 +0200

    fredlerklaarmaken

--- app/src/main/java/com/example/fledler/MainActivity.java
+++ app/src/main/java/com/example/fledler/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.fledler;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.fledler.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main); // Set the layout resource file for the activity
+        // Additional initialization or logic can be added here
     }
 }
