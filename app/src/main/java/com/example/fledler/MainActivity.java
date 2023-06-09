069fddbad9b5a67aa94eafd06774d982f6f7b4b4
mohammedcifci <118259207+mohammedcifci@users.noreply.github.com>
2023-06-09 14:59:25 +0200

    finnishedvoorpagina

--- app/src/main/java/com/example/fledler/MainActivity.java
+++ app/src/main/java/com/example/fledler/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.fledler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.fredler.R;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Set the layout resource file for the activity
         // Additional initialization or logic can be added here
+
+        ImageView settingsImageView = findViewById(R.id.settings);
+        settingsImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform the desired action when the ImageView is clicked
+                // For example, start a new activity or navigate to another project file
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
