10787cf1a117bb9d930d9ac18e04551b39d4c273
mohammedcifci <118259207+mohammedcifci@users.noreply.github.com>
2023-06-12 09:27:07 +0200

    home_settingsAF

--- app/src/main/java/com/example/fledler/SettingsActivity.java
+++ app/src/main/java/com/example/fledler/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.fledler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,13 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        ImageView settingsImageView = findViewById(R.id.back);
+        settingsImageView.setOnClickListener(view -> {
+            // Perform the desired action when the ImageView is clicked
+            // For example, start a new activity or navigate to another project file
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
